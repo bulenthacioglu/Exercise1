45b5e76c60c88540b07ca5bd19c11dfec4a62075
bulenthacioglu <bulent.hacioglu@hotmail.com>
2016-10-24 23:06:07 +0200

    commit msg

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,6 +15,11 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.println("Please enter your name:");
+		Scanner console = new Scanner(System.in);
+		String userName = console.next();
+		HelloUser user = new HelloUser(userName);
+		user.greetUser();
 	}
 
 }

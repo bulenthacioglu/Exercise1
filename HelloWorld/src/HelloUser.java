cd74a54d5e007c6dc2744fe683d39ad53568e72f
bulenthacioglu <bulent.hacioglu@hotmail.com>
2016-10-24 23:10:37 +0200

    hellouser branch

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,10 @@
+
+public class HelloUser {
+	public String userName;
+	public HelloUser(String userName){
+		this.userName = userName;
+	}
+	public void greetUser(){
+		System.out.println("Hello " + userName);
+	}
+}
